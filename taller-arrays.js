2f1e64ace54a8151908471640d2d13566e4a7827
sebastianleon6 <sebastianleon219@gmail.com>
2025-10-30 17:02:40 -0500

    Taller JS

---
+++ taller-arrays.js
@@ -0,0 +1,44 @@
+const prompt = require("prompt-sync")();
+
+/// Ejercicio 1: Calculadora de Propinas ///
+/*
+function calcularPropina (total, porcentaje) {
+    let propina = (total * porcentaje) / 100;
+    let totalFinal = total + propina;
+    return totalFinal;
+};
+
+let totalCuenta = parseInt(prompt("Ingrese el monto de la cuenta:"));
+let porcentajePropina = parseInt(prompt("Ingrese el porcentaje de la propina: "));
+
+let totalPagar = calcularPropina(totalCuenta, porcentajePropina)
+
+console.log("Total de la cuenta mas prominas: " + totalPagar)
+
+/// Ejercicio 2: Validar Contraseña ///
+
+function validarContrasena (contrasena){
+};
+
+/// Ejercicio 3: Buscar Producto en Inventario ///
+
+
+const inventario = [
+    {nombre: "Camisa", precio: "20000", talla: "S, M, L, X, XL"},
+    {nombre: "Pantalon", precio: "50000", talla: "S, M, L, X, XL"},
+    {nombre: "Zapatos", precio: "100000", talla: "30, 33, 37, 40, 42"}
+];
+
+function buscarProducto (nombre, inventario){
+    let producto = inventario.log(nombre)
+}
+
+
+let produc = prompt("Que producto buscas: ");
+console.log(produc(buscarProducto(produc, inventario)));
+*/
+/// Ejercicio 4: Calcular Promedio de Notas ///
+
+function
+
+
